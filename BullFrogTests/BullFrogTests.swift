c06bee5687431f9d0740c3730bdba78439fd3d16
rday <rday@teladochealth.com>
2024-12-18 12:48:38 -0500

    Initial Commit

---
+++ BullFrogTests/BullFrogTests.swift
@@ -0,0 +1,17 @@
+//
+//  BullFrogTests.swift
+//  BullFrogTests
+//
+//  Created by Robert Day on 12/18/24.
+//
+
+import Testing
+@testable import BullFrog
+
+struct BullFrogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
